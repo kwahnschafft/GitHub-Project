49c500a879dd5444b300379305dcb0d482b756d1
kwahnschafft <kwahnschafft@gmail.com>
2015-01-22 15:33:42 -0500

    made all numbers 10s

--- index.php
+++ index.php
@@ -16,15 +16,15 @@
 
 <table>
     <tr>
-        <td><button>1</button></td>
-        <td><button>2</button></td>
-        <td><button>3</button></td>
-        <td><button>4</button></td>
-        <td><button>5</button></td>
-        <td><button>6</button></td>
-        <td><button>7</button></td>
-        <td><button>8</button></td>
-        <td><button>9</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
+        <td><button>10</button></td>
         <td><button>10</button></td>
     </tr>
 </table>
