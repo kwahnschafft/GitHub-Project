df6c0c2a6cebecf5b2432c06f7f6a65423359170
kwahnschafft <kwahnschafft@gmail.com>
2015-01-22 14:30:49 -0500

    Added elements to ReadMe and Index

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Hi Everybody - This is a readMe
+Author: _Kiara Wahnschafft, Torres & Co._
+
+URL:
+
+Date of Deployment: January 22, 2015
+
+___
+
+Special thanks to **Julia McClellan** for her inspirational cats.
+
+Even specialer thanks to **Sananda Chintamani** for her inspirational frogs.
+
+# h1 CHANGE LOG
+
--- index.php
+++ index.php
@@ -1,2 +1,39 @@
-<h1>Hello</h1>
-    <img src="https://octodex.github.com/daftpunktocat-guy/">
+<h1>Project Number...I stopped counting</h1>
+<p>Your computer science class will never be as cool as mine. Deal with it.</p>
+<p>What's that? You wanna know why? Look below.</p>
+<ol>
+  <li>It just is.</li>
+</ol>
+
+<p>Is my computer science class the coolest?</p>
+<form>
+<input type="radio" name="yorn" value="yes1" checked>Yes
+<br>
+<input type="radio" name="yorn" value="yes2">Yes
+</form>
+
+<table>
+    <tr>
+        <td>1</div></td>
+        <td>2</td>
+        <td>3</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+    </tr>
+    <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>0</td>
+        <td></td>
+    </tr>
+</table>
+
+    <img src="daftpunktocat-guy.png">
+        
